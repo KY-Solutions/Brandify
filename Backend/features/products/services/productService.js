6c85ff6b71c33d09b3bf0e291aab1c98873e7298
karim <karim.tarek96@yahoo.com>
2024-11-01 17:51:15 +0200

    created productService.js

---
+++ Backend/features/products/services/productService.js
@@ -0,0 +1,38 @@
+//* import packages
+const Product = require('../models/product');
+
+
+//* Product Service class
+
+class ProductService {
+
+    static async createProduct(productData) {
+        const product = new Product(productData);
+        return await product.save();
+    }
+
+    static async getAllProducts(filters = {},page = 1, limit = 10) {
+        return await Product.find(filters)
+            .populate('Category')
+            .populate('Review')
+            .skip((page - 1) * limit)
+            .limit(limit);
+    }
+
+    static async getProductbyId(productId) {
+        return await Product.findById(productId)
+            .populate('Category')
+            .populate('Review');
+    }
+
+    static async updateProduct(productId, productData) {
+        return await Product.findByIdAndUpdate(productId, productData, { new: true });
+    }
+
+    static async deleteProduct(productId) {
+        return await Product.findByIdAndDelete(productId);
+    }
+
+}
+
+module.exports = ProductService;
