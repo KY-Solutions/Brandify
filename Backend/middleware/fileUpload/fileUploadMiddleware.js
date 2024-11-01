47131934059840f01166b31f7632f200fbc7e606
karim <karim.tarek96@yahoo.com>
2024-11-01 18:28:09 +0200

    added file upload middleware

---
+++ Backend/middleware/fileUpload/fileUploadMiddleware.js
@@ -0,0 +1,29 @@
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+    destination: (req, file, callBackFunc) => {
+        callBackFunc(null, 'uploads/'); //! folder to save images
+    },
+    filename: (req, file, callBackFunc) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        callBackFunc(null, uniqueSuffix + path.extname(file.originalname));
+    }
+});
+
+const fileFilter = (req, file, callBackFunc) => {
+    if (file.mimetype.startWith('image')) {
+        callBackFunc(null, true);
+    } else {
+        callBackFunc(new Error('Only image files are allowed'), false);
+
+    }
+};
+
+const upload = multer({
+    storage,
+    fileFilter,
+    limits: { fileSize: 5 * 1024 * 1024 } //! Limit file size to 5 MB
+});
+
+module.exports = upload;
